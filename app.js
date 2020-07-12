2fee2a865ff8b5ed1f8da458cbbf881561a937ae
SAFNAS <iamsafnas@gmail.com>
2020-07-12 16:20:09 +0530

    added scale script and made svg to render properly

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const DUMMY_DATA = [
     {id:'d4',value:6, region:'Germany'}
 ]
 
-const container = d3.select('div')
+const xScale = d3.scaleBand().domain(DUMMY_DATA.map((datapoint)=>datapoint.region)).rangeRound([0,250]).padding(0.1) // max width of container
+const yScale = d3.scaleLinear().domain([0, 15]).range([200,0]) //max height
+
+const container = d3.select('svg')
 .classed('container',true)
-.style('border','1px solid red')
 
 const bars = container
     .selectAll('.bar')
     .data(DUMMY_DATA)
     .enter()
-    .append('div')
+    .append('rect')
     .classed('bar',true)
-    .style('width','50px')
-    .style('height',data => data.value * 15+'px')
+    .attr('width',xScale.bandwidth())
+    .attr('height',(data) =>200 - yScale(data.value))
+    .attr('x',data=>xScale(data.region))
+    .attr('y',data=>yScale(data.value))
